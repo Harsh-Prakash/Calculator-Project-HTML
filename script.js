895714d29bebbb170678b32170e6ae21efa48797
Harsh-Prakash <138960290+Harsh-Prakash@users.noreply.github.com>
2023-12-29 17:07:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ Array.from(buttons).forEach((button)=>{
       document.querySelector('input').value = string;
     } 
     else{
-    console.log(e.target)
     string = string + e.target.innerHTML;
     document.querySelector('input').value = string;
     }
